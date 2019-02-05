b5e2e1a4fd8f6739f6084eb64a29c7c69eb0d76a
Dominic Michael Silveira <dms24081999@gmail.com>
2019-02-06 00:38:34 +0530

    Updated Hashtag

--- hashtags/views.py
+++ hashtags/views.py
@@ -6,4 +6,5 @@ from .models import HashTag
 class HashTagView(View):
     def get(self,request,hashtag,*args,**kwargs):
         obj,created=HashTag.objects.get_or_create(tag=hashtag)
+        print("Hashtag",created)
         return render(request,'hashtags/tag_view.html',{"obj":obj})
